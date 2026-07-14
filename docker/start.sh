fd2ee91c472786c4182140201eabe4c9c14e0240
UDD L301 PC-15 <taporcotl.686.stud@cdd.edu.ph>
2026-07-14 15:30:34 +0800

    tweak

--- docker/start.sh
+++ docker/start.sh
@@ -2,11 +2,12 @@
 set -e
 
 PORT_TO_USE=${PORT:-80}
-sed -i "s/Listen 80/Listen ${PORT_TO_USE}/" etc/apache2/ports.conf
-sed -i "s/:80/:${PORT_TO_USE}/" etc/apache2/sites-available/000-default.conf
+sed -i "s/Listen 80/Listen ${PORT_TO_USE}/" /etc/apache2/ports.conf
+sed -i "s/:80/:${PORT_TO_USE}/" /etc/apache2/sites-available/000-default.conf
 
 php artisan storage:link || true
+php artisan db:seed || true
 php artisan migrate --force || true
-php artisan optimize
+php artisan optimize || true
 
 apache2-foreground

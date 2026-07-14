9ed915a91c2474a743a23cd671fb1853eea0500f
MelvinViado18 <viadonjg.421.stud@cdd.edu.ph>
2026-07-14 13:40:37 +0800

    final_project

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,19 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\StudentCreated;
 use App\Http\Requests\StoreStudentRequest;
 use App\Http\Requests\UpdateStudentRequest;
 use App\Models\Student;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class StudentController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of students.
      */
-    public function index()
+    public function index(): View
     {
         $students = Student::query()
             ->orderBy('last_name')
@@ -25,19 +25,21 @@ class StudentController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Show the form for creating a new student.
      */
-    public function create()
+    public function create(): View
     {
         return view('students.create');
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Store a newly created student.
      */
     public function store(StoreStudentRequest $request): RedirectResponse
     {
-        Student::create($request->validated());
+        $student = Student::create($request->validated());
+
+        broadcast(new StudentCreated($student));
 
         return redirect()
             ->route('students.index')
@@ -45,15 +47,7 @@ class StudentController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
+     * Show the form for editing the specified student.
      */
     public function edit(Student $student): View
     {
@@ -61,7 +55,7 @@ class StudentController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
+     * Update the specified student.
      */
     public function update(UpdateStudentRequest $request, Student $student): RedirectResponse
     {
@@ -73,7 +67,7 @@ class StudentController extends Controller
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Remove the specified student.
      */
     public function destroy(Student $student): RedirectResponse
     {
